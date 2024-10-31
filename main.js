92d4b8c29693a3d15ee2501055aac2f843792730
Amigo <Sabaka@mail.com>
2024-10-31 18:05:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var you = prompt("введите своё имя :");
+alert (you)
+console.log(you);
+var age = +prompt("введите свой возраст");
+alert(age);
+console.log(age);
+var plus = +prompt("решите первый пример:" + " 10 + 5 = " );
+alert(plus);
+console.log("правельный ответ : " + " 10 + 5 =" +(10+5) + " ваш ответ : " + plus);
+var minus = +prompt ("решите второй пример : " + "10 - 5 =" );
+alert(minus);
+console.log("правельный ответ : " + "10 - 5 =" + (10-5) + " ваш ответ : " + minus);
+var um = +prompt ("решите третий пример : " + "10 * 5 = ")
+console.log("правельный ответ : " + "10 * 5 " + 10*5 + "ваш ответ : " + um);
+var del = +prompt("решите четвёртый пример :" + " 10 / 5 =");
+console.log("правельный ответ : " + "10 / 5 = " + 10/5 + " ваш ответ : " + del)
+var deg =+prompt ("решите последний пример : " + "10 % 5 = ")
+alert(deg);
+console.log("правельный ответ :" + " 10 % 5 = " + 10 % 5 + " ваш ответ : " +deg)
+var X, Y, Z, res;
+X=prompt ('первое число : ');
+Y=prompt ("второе число : ");
+Z=prompt ('третье число : ');
+var res = +prompt("ответ : " + (X+Y+Z)/3)
+console.log('ответ : ' + res);
+var end = ("перейдите в консоль что бы увидеть ответы");
+alert(end);
